cc482563cb4e7bb596c93f1acd6229db4be7cff2
handlessdude <61969421+handlessdude@users.noreply.github.com>
2021-12-07 16:22:55 +0300

    before drop table test

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,7 @@
 class Track < ApplicationRecord
+  validates_presence_of :track_name,
+                        :preview_text,
+                        :preview_picture,
+                        :mode,
+                        :published
 end
