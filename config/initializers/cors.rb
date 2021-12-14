1d0a6b26ed2d8c58d39c6b7a3e96de798c9a9a2c
handlessdude <61969421+handlessdude@users.noreply.github.com>
2021-12-14 19:22:55 +0300

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'
+    origins '*'
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
 
