281940b2765c26158555ff18005dddfad5c9ca42
alator-ux <vavilkin.max@mail.ru>
2021-12-21 15:08:20 +0300

    changed user

--- db/migrate/20211213174126_create_users.rb
+++ db/migrate/20211213174126_create_users.rb
@@ -2,11 +2,12 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :login, null: false
-      t.string :encrypted_password, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
       t.string :first_name, null: false
       t.string :second_name, null: false
       t.string :avatar_url
-      t.references :faculty, null: false
+      t.references :faculty
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -150,11 +150,12 @@ ActiveRecord::Schema.define(version: 2021_12_16_083952) do
 
   create_table "users", force: :cascade do |t|
     t.string "login", null: false
-    t.string "encrypted_password", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.string "first_name", null: false
     t.string "second_name", null: false
     t.string "avatar_url"
-    t.bigint "faculty_id", null: false
+    t.bigint "faculty_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["faculty_id"], name: "index_users_on_faculty_id"
