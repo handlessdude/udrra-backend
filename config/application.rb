110b0b8c34fdf89a1a911df59c3d796beb70b356
handlessdude <61969421+handlessdude@users.noreply.github.com>
2021-12-24 15:19:30 +0300

    fix zeitwerk

--- config/application.rb
+++ config/application.rb
@@ -39,16 +39,8 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoloader = :classic
 
-=begin
-    # Access-Control-Allow-Origin
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'localhost:3000', %r{http://localhost:808?}
-        resource '*', headers: :any, methods: :any
-      end
-    end
-=end
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
